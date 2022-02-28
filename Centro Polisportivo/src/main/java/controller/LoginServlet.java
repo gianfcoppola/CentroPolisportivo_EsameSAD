06fb893be630152fd9f751fcf4866b1f3b465579
Coppola Gianfranco <gianfran.coppola@studenti.unina.it>
2022-02-28 12:08:58 +0100

    final commit

--- Centro Polisportivo/src/main/java/controller/LoginServlet.java
+++ Centro Polisportivo/src/main/java/controller/LoginServlet.java
@@ -124,6 +124,8 @@ public class LoginServlet extends HttpServlet {
 
 		}
 	}
+	
+	
 
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			doGet(request, response);
